dbba691a3534aabc4d07b00bd1781cd55c4ff9bf
Ugochi <amakomitchell5@gmail.com>
2019-12-31 20:17:54 +0100

    resolve environment prod issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+export const localenvironment = {
+  production: false,
+  rootUrl: 'http://localhost:4000'
+};
+
 export const environment = {
   production: false,
   rootUrl: 'https://opensky-network.org/api'
