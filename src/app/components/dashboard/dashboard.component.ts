25c6f0b96626998f3840c86eaee0b8de2793529b
Ugochi <amakomitchell5@gmail.com>
2019-12-31 01:13:27 +0100

    created grid list on dashboard for cities

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
+export interface City {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  cities: City[] = [
+    {text: 'Atlanta', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'New York', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Amsterdam', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'London', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Berlin', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Beijing', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Dubai', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Istanbul', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Shanghai', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Seoul', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
   constructor(
     private router: Router
